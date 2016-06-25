11e1297c3f522ef8fc33983f79da045e4e2e2dfa
Kevin Bohinski <bohinsk1@tcnj.edu>
2016-06-24 21:33:12 -0700

    adding updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,24 @@
 
 ;(function () {
     var el = document.getElementById.bind(document);
-    
+
     el('projects').style.opacity = 0;
     var visible = false;
 
-    el('btn-projects').addEventListener('click', function (e) {
+    function toggleProjects(e) {
         e.preventDefault();
 
         if (!visible) {
+            Fader.fadeOutWithId('btn-ellipsis', 1);
             Fader.fadeInWithId('projects', 1);
         } else {
             Fader.fadeOutWithId('projects', 1);
+            Fader.fadeInWithId('btn-ellipsis', 1);
         }
 
         visible = !visible;
-    }, false);
+    }
+
+    el('btn-projects').addEventListener('click', toggleProjects, false);
+    el('btn-ellipsis').addEventListener('click', toggleProjects, false);
 })();
