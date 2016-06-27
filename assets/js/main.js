174797bb2c3fc9b6ddddbb187a23aadcbdd6ea7f
Kevin Bohinski <bohinsk1@tcnj.edu>
2016-06-26 20:33:44 -0700

    fixing some mobile awkwardness

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,18 +8,22 @@ smoothScroll.init({
     var el = document.getElementById.bind(document);
 
     el('projects').style.opacity = 0;
+    var content = el('projects').innerHTML;
+    el('projects').innerHTML = '';
     var visible = false;
 
     function toggleProjects(e) {
         e.preventDefault();
 
         if (!visible) {
+            el('projects').innerHTML = content;
             smoothScroll.animateScroll('#projects');
             Fader.fadeOutWithId('btn-ellipsis', 1);
             Fader.fadeInWithId('projects', 1);
         } else {
             Fader.fadeOutWithId('projects', 1);
             Fader.fadeInWithId('btn-ellipsis', 1);
+            el('projects').innerHTML = '';
         }
 
         visible = !visible;
