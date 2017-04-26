e692e551d23d250a0724203b08eb7006930f0be4
Kevin Bohinski <kbohinski@gmail.com>
2017-04-26 18:08:53 -0400

    updates for graduation time

--- _RootsToFirebase.sh
+++ _RootsToFirebase.sh
@@ -7,4 +7,9 @@ cd ../_firebase
 rm -rf public
 cp -r ../personal-site-2016/public/ ./public/
 
+rm -rf public/posts
+rm -rf public/_firebase.json
+
 firebase deploy
+
+tree public
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,33 @@
-'use strict'
+'use strict';
 
 smoothScroll.init({
   speed: 500,
   easing: 'easeInOutCubic'
-})
+});
 
-(() => {
-  let el = document.getElementById.bind(document)
+;(function () {
+  var el = document.getElementById.bind(document);
 
-  el('projects').style.opacity = 0
-  let content = el('projects').innerHTML
-  el('projects').innerHTML = ''
-  let visible = false
+  el('projects').style.opacity = 0;
+  var content = el('projects').innerHTML;
+  el('projects').innerHTML = '';
+  var visible = false;
 
-  el('btn-projects').addEventListener('click', (e) => {
-    e.preventDefault()
+  function toggleProjects (e) {
+    e.preventDefault();
 
     if (!visible) {
-      el('projects').innerHTML = content
-      smoothScroll.animateScroll('#projects')
-      Fader.fadeInWithId('projects', 1)
+      el('projects').innerHTML = content;
+      smoothScroll.animateScroll('#projects');
+      Fader.fadeInWithId('projects', 1);
     } else {
-      Fader.fadeOutWithId('projects', 1)
-      smoothScroll.animateScroll('body')
-      setTimeout("document.getElementById('projects').innerHTML = ''", 1000)
+      Fader.fadeOutWithId('projects', 1);
+      smoothScroll.animateScroll('body');
+      setTimeout("document.getElementById('projects').innerHTML = '';", 1000);
     }
 
-    visible = !visible
-  }, false)
-})()
+    visible = !visible;
+  }
+
+  el('btn-projects').addEventListener('click', toggleProjects, false);
+})();
