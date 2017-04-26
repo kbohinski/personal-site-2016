570f6b2a87399210c7a3d680c4b89758d6614bda
Kevin Bohinski <kbohinski@gmail.com>
2017-04-26 17:23:27 -0400

    small changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,31 @@
-'use strict';
+'use strict'
 
 smoothScroll.init({
-    speed: 500,
-    easing: 'easeInOutCubic'
-});
+  speed: 500,
+  easing: 'easeInOutCubic'
+})
 
-;(function () {
-    var el = document.getElementById.bind(document);
+(() => {
+  let el = document.getElementById.bind(document)
 
-    el('projects').style.opacity = 0;
-    var content = el('projects').innerHTML;
-    el('projects').innerHTML = '';
-    var visible = false;
+  el('projects').style.opacity = 0
+  let content = el('projects').innerHTML
+  el('projects').innerHTML = ''
+  let visible = false
 
-    function toggleProjects(e) {
-        e.preventDefault();
+  el('btn-projects').addEventListener('click', (e) => {
+    e.preventDefault()
 
-        if (!visible) {
-            el('projects').innerHTML = content;
-            smoothScroll.animateScroll('#projects');
-            Fader.fadeInWithId('projects', 1);
-        } else {
-            Fader.fadeOutWithId('projects', 1);
-            smoothScroll.animateScroll('body');
-            setTimeout("document.getElementById('projects').innerHTML = '';", 1000);
-        }
-
-        visible = !visible;
+    if (!visible) {
+      el('projects').innerHTML = content
+      smoothScroll.animateScroll('#projects')
+      Fader.fadeInWithId('projects', 1)
+    } else {
+      Fader.fadeOutWithId('projects', 1)
+      smoothScroll.animateScroll('body')
+      setTimeout("document.getElementById('projects').innerHTML = ''", 1000)
     }
 
-    el('btn-projects').addEventListener('click', toggleProjects, false);
-})();
+    visible = !visible
+  }, false)
+})()
